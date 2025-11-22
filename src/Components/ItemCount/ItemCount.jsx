b044eef214a5e727be58747624489c7715ff93b5
Eliana Montiel <elianamontiel_10@hotmail.com>
2025-11-22 08:38:20 -0300

    Se agrego itemCount y tambien el stock de productos en data

---
+++ src/Components/ItemCount/ItemCount.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "./itemcount.css";
+
+const ItemCount = ({ stock, addToCart }) => {
+  const [count, setCount] = useState(1);
+
+  const handleDecrement = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    }
+  };
+
+  const handleIncrement = () => {
+    if (count < stock) {
+      setCount(count + 1);
+    }
+  };
+
+  return (
+    <div className="item-count">
+      <div className="controls-count">
+        <button onClick={handleDecrement} >-</button>
+        <p>{count}</p>
+        <button onClick={handleIncrement} >+</button>
+      </div>
+      <button className="button-add-count" onClick={() => addToCart(count)} >Agregar producto</button>
+    </div>
+  )
+}
+
+export default ItemCount
--- src/Data/Products.js
+++ src/Data/Products.js
@@ -11,16 +11,16 @@ import superCombo from "../assets/superCombo.jpg"
 
 
 const products = [
-  { id: 1, name: "Labial Velvet Rojo", price: 1200, category: "Labiales", img: labialRojo },
-  { id: 2, name: "Gloss Brillo Intenso", price: 1500, category: "Gloss", img: gloss },
-  { id: 3, name: "Paleta Dorada", price: 2200, category: "Paletas", img: paletaDorada },
-  { id: 4, name: "Combo Glam", price: 3500, category: "Combos", img: combo3 },
-  { id: 5, name: "Combo Nude", price: 4000, category: "Combos", img: comboNude },
-  { id: 6, name: "Labiales", price: 4000, category: "Labiales", img: labiales },
-  { id: 7, name: "Paleta de colores", price: 2000, category: "Paletas", img: paletas },
-  { id: 8, name: "Gloss Nude", price: 2000, category: "Gloss", img: glossNude },
-  { id: 9, name: "Paleta cuatro colores", price: 2000, category: "Paletas", img: paleta4 },
-  { id: 10, name: "Super combo", price: 5000, category: "Combos", img: superCombo }
+  { id: 1, name: "Labial Velvet Rojo", price: 1200, category: "Labiales", img: labialRojo, stock: 10},
+  { id: 2, name: "Gloss Brillo Intenso", price: 1500, category: "Gloss", img: gloss, stock: 15},
+  { id: 3, name: "Paleta Dorada", price: 2200, category: "Paletas", img: paletaDorada, stock: 5 },
+  { id: 4, name: "Combo Glam", price: 3500, category: "Combos", img: combo3, stock: 4},
+  { id: 5, name: "Combo Nude", price: 4000, category: "Combos", img: comboNude, stock: 4},
+  { id: 6, name: "Labiales", price: 4000, category: "Labiales", img: labiales, stock: 7},
+  { id: 7, name: "Paleta de colores", price: 2000, category: "Paletas", img: paletas, stock: 2},
+  { id: 8, name: "Gloss Nude", price: 2000, category: "Gloss", img: glossNude, stock: 5},
+  { id: 9, name: "Paleta cuatro colores", price: 2000, category: "Paletas", img: paleta4, stock: 5},
+  { id: 10, name: "Super combo", price: 5000, category: "Combos", img: superCombo, stock: 8}
 ];
 
 export default products;
